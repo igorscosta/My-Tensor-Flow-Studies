cdb5e6e019a7b7cb2906906c3237a2c020e77b0a
Igor Costa <igorsc93@gmail.com>
2018-05-11 18:39:16 -0300

    Running multiple sessions with multiple operations

--- tensorflow_basic_syntax.py
+++ tensorflow_basic_syntax.py
@@ -23,3 +23,17 @@ with tf.Session() as session:
     result_2 = session.run(a + b)
     print(type(result_2))
     print(result_2)
+
+const = tf.constant(10)
+fill_mat = tf.fill(((4,4)), 10)
+my_zeros = tf.zeros((4,4))
+my_ones = tf.ones((4,4))
+my_random_n = tf.random_normal((4,4), mean = 0, stddev = 1.0)
+my_random_u = tf.random_uniform((4,4), minval = 0, maxval = 1)
+
+my_ops = [const, fill_mat, my_zeros, my_ones, my_random_n, my_random_u]
+
+for operation in my_ops:
+    with tf.Session() as session:
+        print(session.run(operation))
+    print('\n')
