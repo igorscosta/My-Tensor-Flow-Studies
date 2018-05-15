76841d5085ae5cdd51cf51ac781a7b09b63fafe5
Igor Costa <igorsc93@gmail.com>
2018-05-15 12:03:05 -0300

    Very simple and basic Neural Network Example

--- neural_network.py
+++ neural_network.py
@@ -24,5 +24,23 @@ with tf.Session() as sess:
     print(mult_result)
 
 
+############## Example Neural Network ####################
 
+n_features = 10
+n_dense_neurons = 3
+ 
+x = tf.placeholder(tf.float32, (None, n_features))
+W = tf.Variable(tf.random_normal([n_features, n_dense_neurons]))
+b = tf.Variable(tf.ones([n_dense_neurons]))
+xW = tf.matmul(x,W)
+z = tf.add(xW,b)
+a = tf.sigmoid(z)
 
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+
+    sess.run(init)
+
+    layer_out = sess.run(a, feed_dict={x:np.random.random([1, n_features])})
+
+    print(layer_out)
