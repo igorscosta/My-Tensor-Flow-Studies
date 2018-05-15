952681001f15a9c79e27ad9eb964a6f7fb6a3c2b
Igor Costa <igorsc93@gmail.com>
2018-05-15 14:45:28 -0300

    Running a simple regression into a Neural Network

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf 
+import matplotlib.pyplot as plt
 
 np.random.seed(101)
 tf.set_random_seed(101)
@@ -44,3 +45,54 @@ with tf.Session() as sess:
     layer_out = sess.run(a, feed_dict={x:np.random.random([1, n_features])})
 
     print(layer_out)
+
+print('\n')
+
+################ Simple Regression Example ##################
+
+x_data = np.linspace(0,10,10) + np.random.uniform(-1.5,1.5,10)
+print(x_data)
+print('\n')
+
+y_label = np.linspace(0,10,10) + np.random.uniform(-1.5,1.5,10)
+print(y_label)
+
+plt.plot(x_data, y_label, '*')
+plt.show()
+
+######## y = mx + b #########
+
+m = tf.Variable(0.44)
+b = tf.Variable(0.87)
+
+error = 0
+
+for x,y in zip(x_data, y_label):
+
+    y_hat = m * x + b
+
+    error += (y - y_hat) ** 2
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.001)
+train = optimizer.minimize(error)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+
+    sess.run(init)
+    training_steps = 100
+
+    for i in range(training_steps):
+        sess.run(train)
+
+    final_slope, final_intercept = sess.run([m, b])
+
+x_test = np.linspace(-1,11,10)
+
+# y = mx + b
+y_pred_plot = final_slope * x_test + final_intercept
+
+plt.plot(x_test, y_pred_plot, 'r')
+plt.plot(x_data, y_label, '*')
+plt.show()
