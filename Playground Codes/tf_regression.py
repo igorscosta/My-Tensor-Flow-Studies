318de326356a48efffdbe5f07c5daf436cc9d8ee
Igor Costa <igorsc93@gmail.com>
2018-10-02 18:06:19 -0300

    Playing with a regretion using random data

---
+++ Playground Codes/tf_regression.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+
+#Generating some random data
+
+x_data = np.linspace(0.0,10.0,1000000)
+noise = np.random.randn(len(x_data))
+
+y_true = (0.5 * x_data) + 5 + noise
+
+x_df = pd.DataFrame(data=x_data, columns = ['X Data'])
+y_df = pd.DataFrame(data=y_true, columns = ['Y Data'])
+
+
+my_data = pd.concat([x_df, y_df], axis = 1)
+print(my_data.head())
+
