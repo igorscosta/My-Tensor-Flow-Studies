ed830701eb01b45c749f6cdfb622903f49f6727f
Igor Costa <igorsc93@gmail.com>
2018-10-02 18:16:13 -0300

    Ploting some sample data

--- Playground Codes/tf_regression.py
+++ Playground Codes/tf_regression.py
@@ -11,9 +11,15 @@ noise = np.random.randn(len(x_data))
 y_true = (0.5 * x_data) + 5 + noise
 
 x_df = pd.DataFrame(data=x_data, columns = ['X Data'])
-y_df = pd.DataFrame(data=y_true, columns = ['Y Data'])
+y_df = pd.DataFrame(data=y_true, columns = ['Y'])
 
 
 my_data = pd.concat([x_df, y_df], axis = 1)
 print(my_data.head())
+
+#Ploting a sample data
+
+my_data.sample(n = 250).plot(kind = 'scatter', x = 'X Data', y = 'Y')
+plt.show()
+
 
