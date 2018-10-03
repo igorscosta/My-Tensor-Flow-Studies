93e075e0736d33f419be415bae44d5229de2a0bb
Igor Costa <igorsc93@gmail.com>
2018-10-03 16:54:29 -0300

    Regression example using scatter plot

--- Playground Codes/tf_regression.py
+++ Playground Codes/tf_regression.py
@@ -17,9 +17,50 @@ y_df = pd.DataFrame(data=y_true, columns = ['Y'])
 my_data = pd.concat([x_df, y_df], axis = 1)
 print(my_data.head())
 
-#Ploting a sample data
+#Ploting some sample data
 
 my_data.sample(n = 250).plot(kind = 'scatter', x = 'X Data', y = 'Y')
 plt.show()
 
+# A million entries is too much for a Neural Network so..
+#Creating some batches of data
+batch_size = 8 
 
+#Declaring some variables
+m = tf.Variable(0.81)
+b = tf.Variable(0.17)
+
+#Declaring some placeholders
+xph = tf.placeholder(tf.float32, [batch_size])
+yph = tf.placeholder(tf.float32, [batch_size])
+
+y_model = m*xph + b
+
+#Loss Function
+error = tf.reduce_sum(tf.square(yph-y_model))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train = optimizer.minimize(error)
+
+#initializing the variables
+init = tf.global_variables_initializer()
+
+#creating a TF session
+with tf.Session() as sess:
+    sess.run(init)
+    batches = 1000
+
+    for i in range(batches):
+
+        rand_ind = np.random.randint(len(x_data), size = batch_size)
+
+        feed = {xph : x_data[rand_ind], yph : y_true[rand_ind]}
+
+        sess.run(train, feed_dict = feed)
+
+    model_m, model_b = sess.run([m, b])
+
+y_hat = x_data * model_m + model_b
+my_data.sample(250).plot(kind = 'scatter', x = 'X Data', y = 'Y')
+plt.plot(x_data, y_hat, 'r')
+plt.show()
